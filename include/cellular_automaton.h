2827dd44031e43fac0c115e57f4b467dffb7c0ec
ImadElMaftouhi <imad.elmaft.wrk@gmail.com>
2025-10-22 01:04:30 +0100

    expanded on methods

--- include/cellular_automaton.h
+++ include/cellular_automaton.h
@@ -13,6 +13,16 @@ private:
 public:
     CellularAutomaton(size_t grid_size, uint32_t rule_number);
     void init_state(const std::vector<int>& initial_state);//init a vector of bit
+    void init_single_center(); //init state from a single center cell
+    void evolve(); //evolve one generation
+    void evolve_setps(size_t steps); //run multiple
+    std::vector<int> get_state() const; 
+    int get_cell(size_t index) const;
+    void set_rule(uint32_t rule_number);
+    uint32_t get_rule() const;
+    void print_state();
+    std::string state_to_string() const; //get state as binary string
+    void reset(); //all zeros
 private:
     int apply_rule(int left, int center, int right) const;//apply rule to a 3-cell neighborhood
 };
